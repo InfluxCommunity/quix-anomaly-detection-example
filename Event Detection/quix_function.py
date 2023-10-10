595a2f6849ba706155a897a7a4723addc2b44a89
Jay Clifford <jclifford@influxdata.com>
2023-10-10 13:34:04 +0000

    Edited 'quix_function.py'

--- Event Detection/quix_function.py
+++ Event Detection/quix_function.py
@@ -1,12 +1,16 @@
 import quixstreams as qx
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import os
 
 
 class QuixFunction:
-    def __init__(self, consumer_stream: qx.StreamConsumer, producer_stream: qx.StreamProducer):
+    def __init__(self, consumer_stream: qx.StreamConsumer, producer_stream: qx.StreamProducer, model):
         self.consumer_stream = consumer_stream
         self.producer_stream = producer_stream
-
+        # Load the autoencoder model from the file
+        self.model = model
     # Callback triggered for each new event
     def on_event_data_handler(self, stream_consumer: qx.StreamConsumer, data: qx.EventData):
         print(data.value)
@@ -15,20 +19,34 @@ class QuixFunction:
 
         self.producer_stream.events.publish(data)
 
-    # Callback triggered for each new timeseries data
+    # Callback triggered for each new parameter data.
     def on_dataframe_handler(self, stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
-        
-        output_df = pd.DataFrame()
-        output_df["timestamp"] = df["timestamp"]
+# Normalize the anomalous data
+        print(df)
+        df = df.set_index('timestamp')
+        anom_data = df.drop(columns=['iox::measurement', 'machineID'])
 
-        if "TAG__LapNumber" in df.columns:
-            output_df["TAG__LapNumber"] = df["TAG__LapNumber"]
-            
+
+        # Use the Autoencoder to predict on the anomalous data
+        predictions = self.model.predict(anom_data)
+
+        # Calculate reconstruction error
+        mse = np.mean(np.power(anom_data.values - predictions, 2), axis=1)
+
+        # Scale the MSE to a percentage
+        min_mse = np.min(mse)
+        max_mse = np.max(mse)
+        mse_percentage = ((mse - min_mse) / (max_mse - min_mse)) * 100
+
+        # Detect anomalies by comparing the scaled reconstruction error to some threshold
+        threshold = float(os.environ["threshold"])  # Define a threshold value (in percentage)
+
+        # Add 'is_anomalous' column to the DataFrame
+        df['is_anomalous'] = mse_percentage > threshold
+        df['mse_percentage'] = mse_percentage
+
+        df = df.reset_index().rename(columns={'timestamp': 'time'})
         print(df)
 
-        # If braking force applied is more than 50%, we send True.
-        # update this code to apply your own logic or ML model processing
-        if "Brake" in df.columns:
-            output_df["HardBraking"] = df.apply(lambda row: "True" if row.Brake > 0.5 else "False", axis = 1)  
 
-        self.producer_stream.timeseries.buffer.publish(output_df)
+        self.producer_stream.timeseries.buffer.publish(df)  # Send filtered data to output topic
